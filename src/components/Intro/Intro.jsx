fa211ea182479ac1b0953b6afc80e947d9608beb
AmoghCreator <amoghpreneur@gmail.com>
2024-02-20 19:33:50 +0530

    responsive fix

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,16 +2,16 @@ import './Intro.css';
 
 function Intro(props) {
   return (
-	<div className="intro">
-	    {[...Array(7)].map((_, i) => (
-		(i <= 3) ? <h1 style={{opacity : 0.25 + i*.25}}> Hi, I am Amogh </h1> 
-						// ^ starting from 0.25 opacity, increase by 0.25 each time 
-			 : <h1 style={{opacity : 0.75 - (i-4) * .25}}> Hi, I am Amogh </h1> 
-						// starting from 0.75 opacity, decrease by 0.25 each time ,
-						// i-4 because we want the 4 the element to be 0.75, and 
-						// decrease in opacity subsequently
-	    ))}
-	</div>
+    <div className="intro">
+      {[...Array(7)].map((_, i) => (
+        (i <= 3) ? <h1 style={{ opacity: 0.25 + i * .25 }}> Hi, I am Amogh </h1>
+          // ^ starting from 0.25 opacity, increase by 0.25 each time 
+          : <h1 style={{ opacity: 0.75 - (i - 4) * .25 }}> Hi, I am Amogh </h1>
+        // starting from 0.75 opacity, decrease by 0.25 each time ,
+        // i-4 because we want the 4 the element to be 0.75, and 
+        // decrease in opacity subsequently
+      ))}
+    </div>
   );
 }
 
--- src/components/OnHoverNav/OnHoverNav.jsx
+++ src/components/OnHoverNav/OnHoverNav.jsx
@@ -1,9 +1,11 @@
 import './onHoverNav.css';
 
 function OnHoverNav(props) {
-    return (
-	<h3 id="hoverDescNav" style={{color:"red"}}>{props.name}</h3>
-    );
+  return (
+    <div className="hoverDescContainer">
+      <h3 id="hoverDescNav" style={{ color: "red" }}>{props.name}</h3>
+    </div>
+  );
 }
 
 export default OnHoverNav;
